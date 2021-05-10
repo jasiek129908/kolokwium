a40a6ac6a0fe4d760d36cba89eefa965b17a1582
Jan Oleksik <222529@edu.p.lodz.pl>
2021-05-10 18:02:09 +0200

    Dodanie testu 2

--- testsReactor/src/test/java/edu/iis/mto/coffee/CoffeeMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/coffee/CoffeeMachineTest.java
@@ -1,6 +1,7 @@
 package edu.iis.mto.coffee;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -14,6 +15,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @ExtendWith(MockitoExtension.class)
 class CoffeeMachineTest {
 
@@ -23,15 +27,21 @@ class CoffeeMachineTest {
     private MilkProvider milkProvider;
     @Mock
     private CoffeeReceipes coffeeReceipes;
-    private CoffeeMachine coffeeMachine;
 
+    private CoffeeMachine coffeeMachine;
+    private Map<CoffeeSize, Integer> waterAmounts= new HashMap<>(){{
+        put(CoffeeSize.SMALL,5);
+        put(CoffeeSize.STANDARD,10);
+        put(CoffeeSize.DOUBLE,20);
+    }};
     @BeforeEach
     void setUp() throws Exception {
         //coffeeMachine = new CoffeeMachine(coffeeGrinder, milkProvider, coffeeReceipes);
+
     }
 
     @Test
-    void MethodMakeShouldThrowCoffeeMachineExceptionWhenThereIsNoRecipe() {
+    void methodMakeShouldThrowCoffeeMachineExceptionWhenThereIsNoRecipe() {
         coffeeMachine = new CoffeeMachine(coffeeGrinder, milkProvider, coffeeReceipes);
         //when(coffeeReceipes.getReceipe(any(CoffeeType.class))).thenReturn(null);
         CoffeeOrder coffeeOrder = CoffeeOrder.builder()
@@ -40,6 +50,21 @@ class CoffeeMachineTest {
                 .build();
         Assertions.assertThrows(CoffeeMachineException.class,()->coffeeMachine.make(coffeeOrder));
     }
-    
 
+    @Test
+    void shouldThrowCoffeeMachineExceptionWhenThereOrderedCoffeeSizeIsNonSpecified() {
+        coffeeMachine = new CoffeeMachine(coffeeGrinder, milkProvider, coffeeReceipes);
+        CoffeeOrder coffeeOrder = CoffeeOrder.builder()
+                .withSize(CoffeeSize.SMALL)
+                .withType(CoffeeType.ESPRESSO)
+                .build();
+
+        CoffeeReceipe coffeeReceipe = CoffeeReceipe.builder()
+                .withMilkAmount(10)
+                .withWaterAmounts(waterAmounts)
+                .build();
+
+        when(coffeeReceipes.getReceipe(any(CoffeeType.class))).thenReturn(coffeeReceipe);
+        Assertions.assertThrows(CoffeeMachineException.class,()->coffeeMachine.make(coffeeOrder));
+    }
 }
